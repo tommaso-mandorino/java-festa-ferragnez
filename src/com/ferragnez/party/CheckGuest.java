2ab19e106fb199a5b3c425b6c9eb1db81cb5f545
tommaso-mandorino <tommasomandorino@outlook.it>
2025-12-04 01:44:35 +0100

    feat: add checking guest feature

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,11 +1,36 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+        String[] guests = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
+
+        System.out.print("Come ti chiami? ");
+        String name = scanner.nextLine();
+        scanner.close();
+
+        boolean guestFound = false;
+
+        int i;
+        for (i = 0; i < guests.length; i++) {
+            if (name.equals(guests[i])) {
+                guestFound = true;
+                break;
+            }
+        }
         
+        if (guestFound) {
+            System.out.println("Benvenuto/a alla festa, " + guests[i] + '!');
+        } else {
+            System.out.println("Ci dispiace " + name + ", ma non sei tra gli invitati della festa!");
+        }
+
     }
 
 }
